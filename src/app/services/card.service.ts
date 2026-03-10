f07fa7f6d02bb871bdb74e3e2107e5d303ad28d0
Arthur <archi.sb.pochta@gmail.com>
2026-03-10 11:24:54 +0300

    feat: get products by user id

--- src/app/services/card.service.ts
+++ src/app/services/card.service.ts
@@ -1,9 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { computed, inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { map, Observable } from 'rxjs';
+import { forkJoin, map, Observable, of } from 'rxjs';
 import { UsersResponse } from '../models/users-response.model';
-import { BankCard, Card, cardStatusesArray } from '../models/user.model';
+import { BankCard, Card, cardStatusesArray, User } from '../models/user.model';
+import { CartsResponse } from '../models/carts-response.model';
+import { Cart } from '../models/cart.model';
 
 @Injectable({
   providedIn: 'root',
@@ -15,9 +17,9 @@ export class CardService {
   private readonly statuses = cardStatusesArray;
 
   getCards(): Observable<Card[]> {
-    return this.http.get<any>(`${this.apiUrl}/users`).pipe(
+    return this.http.get<UsersResponse>(`${this.apiUrl}/users`).pipe(
       map((resp) => {
-        const rawCards: BankCard[] = resp.users.map((user: any) => user.bank);
+        const rawCards: BankCard[] = resp.users.map((user: User) => user.bank);
         return rawCards.map((card, index) => ({
           ...card,
           id: index + 1,
@@ -25,6 +27,21 @@ export class CardService {
         }));
       })
     );
+  }
+
+  getDataByCardId(id: number): Observable<any> {
+    const userData$ = this.http.get<User>(`${this.apiUrl}/users/${id}`).pipe(
+      map(user => ({ username: user.username, bank: user.bank }))
+    );
+  
+    const products$ = this.http.get<CartsResponse>(`${this.apiUrl}/carts/user/${id}`).pipe(
+      map(resp => resp.carts.flatMap(cart => cart.products))
+    );
+  
+    return forkJoin({
+      user: userData$,
+      products: products$
+    });
   }
 
   private getRandomStatus(): 'active' | 'error' | 'expired' | 'suspended' {
